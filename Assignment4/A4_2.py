f46733aabe9fba18b4db4e046493f75ecfab042b
Vishal Kumbhar <86111799+vishalkumbhar18@users.noreply.github.com>
2022-10-03 21:44:50 +0530

    Add files via upload

---
+++ Assignment4/A4_2.py
@@ -0,0 +1,33 @@
+#6
+a=int(input("Enter a length"))
+b=int(input("Enter a breadth"))
+c=int(input("Enter a widtth"))
+print("Area of a Triangle is=",a*b*c)
+print()
+
+#7
+d=int(input("Enter a first number"))
+e=int(input("Enter a second number"))
+f=int(input("Enter a third number"))
+print(((d+e+f)/3))
+print()
+#8
+i=int(input("final amount="))
+j=int(input("initial principal balance"))
+k=int(input("annual interest rate"))
+l=int(input("time in years"))
+i=j*(1+k*l)
+print(i)
+print()
+#9
+m=int(input("Enter a length"))
+n=int(input("Enter a height"))
+o=int(input("Enter a widtth"))
+print("Area of a Cuboid =",m*n*o)
+
+
+#10
+g=int(input("Enter a length"))
+h=int(input("Enter a breadth"))
+print("Area of a Rectangle is=",g*h)
+print()
